56969b124d0085d8dc4c72bd840b7e9b8fdcad53
Ed Shackleton <eddieshackleton@btinternet.com>
2019-01-04 13:10:03 +0000

    initial commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,32 @@
+//initialising map and adding geocoding
+
+var geocoder;
+var map;
+
+function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(51.528, -0.381);
+    var mapOptions = {
+        zoom: 8,
+        center: latlng
+    }
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+
+function codeAddress() {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({ 'address': address }, function(results, status) {
+        if (status == 'OK') {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+        }
+        else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
+
+//show accommodation on button click
